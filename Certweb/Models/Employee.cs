61771f40bc8c6a20bbabf82c9c845afcae0794fd
VictorLCosta <victorlc2019@outlook.com>
2020-10-04 17:21:32 -0300

    Employee model added

---
+++ Certweb/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certweb.Models
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+        public string Gender { get; set; }
+        public string ContractType { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
